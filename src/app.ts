0d0736f17da62d147abc7bab5c905c7fb2cf2a4b
alzuhara <ebautista@iwa.com.mx>
2017-08-09 11:43:28 -0500

    Parámetros y tipos de retorno

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
-type ValidSymbol = '#' | '$'
-const symbol: ValidSymbol = '$'
-var length: number = 7
+type ValidSymbol = '#' | '$';
 
-function generateRandomId() {
+function generateRandomId(symbol: ValidSymbol, length: number): string {
     return symbol + Math.random().toString(36).substr(2, length)
 }
 
 function main() {
     var app = document.getElementById('app')
     setInterval(function() {
-        app.innerHTML = generateRandomId()
+        app.innerHTML = generateRandomId('#', 7)
     }, 1000)
 }
