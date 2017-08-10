9eac00c4a8c991d5f03fcd566e56267f31185735
Emanuel <ebautista@iwa.com.mx>
2017-08-10 16:20:43 -0500

    Métodos de clases

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,23 @@ import { generateRandomId } from './utils'
 
 class App {
     static id = 'app'
+
+    onInit(el: HTMLElement | null): void {
+        setInterval(function() {
+            if(el) {
+                el.innerHTML = generateRandomId({
+                    symbol: '#',
+                    length: 7
+                })
+            }
+        }, 1000)
+    }
 }
 
 function main(ComponentClass) {
-    var app = document.getElementById(ComponentClass.id)
-    setInterval(function() {
-        if(app) {
-            app.innerHTML = generateRandomId({
-                symbol: '#',
-                length: 7
-            })
-        }
-    }, 1000)
+    const el = document.getElementById(ComponentClass.id)
+    const cmp = new ComponentClass()
+    cmp.onInit(el)
 }
 
 main(App)
